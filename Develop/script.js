0683e5e6c84e8d5d61d00dab7455352dece534df
alexoserna <118146045+alexoserna@users.noreply.github.com>
2023-04-24 23:07:02 -0400

    Added all of the html to fill out the hours from 9-5pm

--- Develop/script.js
+++ Develop/script.js
@@ -20,4 +20,7 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+  // Get the current day and display it in the header
+
 });
